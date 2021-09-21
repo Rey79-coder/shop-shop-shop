3ac0690b25f09f91706f6b4adf71308646563d75
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-09-21 02:56:29 -0500

    detail-product-IndexedDB-working

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -16,6 +16,9 @@ import {
 
 import Cart from '../components/Cart';
 
+import { idbPromise } from "../utils/helpers";
+
+
 function Detail() {
 
   const [state, dispatch] = useStoreContext();
@@ -53,15 +56,31 @@ function Detail() {
   };
 
   useEffect(() => {
+    // already in global store
     if (products.length) {
       setCurrentProduct(products.find(product => product._id === id));
-    } else if (data) {
+    } 
+    // retrieved from server
+    else if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+  
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
     }
-  }, [products, data, dispatch, id]);
+    // get cache from idb
+    else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
 
   return (
     <>
